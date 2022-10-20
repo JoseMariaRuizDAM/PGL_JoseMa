50c3d9e88c0a366172c89d80fdcfa49137c0c0d7
Jose Maria RUiz <baifoworldies@gmail.com>
2022-10-20 20:18:19 +0100

    subida ejercicio clase

---
+++ PruebasApp/app/src/main/java/com/example/pruebasapp/Login.java
@@ -0,0 +1,21 @@
+package com.example.pruebasapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo);
+    }
+
+    public void doLogin(View view){
+
+        Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ PruebasApp/app/src/main/java/com/example/pruebasapp/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.pruebasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtGuardar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtGuardar = findViewById(R.id.txtGuardar);
+        String[] archivos = fileList();
+
+        if(exist (archivos, "notas.txt")){
+            try{
+                InputStreamReader archivo = new InputStreamReader(openFileInput("notas.txt"));
+                BufferedReader br = new BufferedReader(archivo);
+                String linea = br.readLine();
+                String todo = "";
+                while(linea != null){
+                    todo = todo + linea + "\n";
+                    linea = br.readLine();
+                }
+                br.close();
+                archivo.close();
+                txtGuardar.setText(todo);
+
+            }catch(IOException e){
+                e.getMessage();
+            }
+        }
+    }
+
+    private boolean exist(String[] archivos, String archivo){
+        for(int i = 0; i < archivos.length; i++){
+            if(archivo.equals(archivos[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void escribirArchivo(View view){
+        try {
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput("notas.txt", Activity.MODE_PRIVATE));
+            archivo.write(txtGuardar.getText().toString());
+            archivo.flush();
+            archivo.close();
+        } catch(IOException e){
+
+        }
+        Toast t = Toast.makeText(this, "Los datos fueron grabados",Toast.LENGTH_SHORT);
+        t.show();
+        finish();
+    }
+
+    public void Siguiente(View view){
+
+        setContentView(R.layout.activity_nuevo);
+    }
+
+    public void Salir(View view){
+
+        cerrarAplicacion();
+
+    }
+
+
+
+    public void Informacion(View view){
+
+    }
+
+    private void cerrarAplicacion() {
+        new AlertDialog.Builder(this)
+                //.setIcon(R.drawable.alacran)
+                .setTitle("¿Realmente desea cerrar la aplicación?")
+                .setCancelable(false)
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {// un listener que al pulsar, cierra la aplicacion
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                        //finish(); Si solo quiere mandar la aplicación a segundo plano
+                    }
+                }).show();
+    }
+}
